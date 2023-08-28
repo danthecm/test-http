39f69f453b1051cd3893f2021ae4def89f6420b1
danthecm <etchiedaniel1@gmail.com>
2023-08-28 15:44:35 +0100

    feat: created breeds endpoint to fetch all breads and group by country

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,28 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/breeds', function () {
+    $result = [];
+    getAllBreeds('https://catfact.ninja/breeds', $result);
+    return $result;
+
+});
+
+function getAllBreeds(string $url, array &$result): void
+{
+    $response = Http::get($url);
+
+    if ($response['next_page_url'] == null) {
+        return;
+    }
+    $data = $response->json()['data'];
+    foreach ($data as $value) {
+        if (!isset($result[$value["country"]])) {
+            $result[$value["country"]] = [];
+        }
+        array_push($result[$value["country"]], $value["breed"]);
+    }
+
+    getAllBreeds($response['next_page_url'], $result);
+}
